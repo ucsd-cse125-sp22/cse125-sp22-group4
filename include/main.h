54e44628b2b2a328fd16e3a4616cfd28c8bcc46a
Jeff Xu / cxcharlie <jjx003@ucsd.edu>
2022-04-05 23:02:31 -0700

    Remove bindings

--- include/main.h
+++ include/main.h
@@ -4,8 +4,8 @@
 
 #include "Client.h"
 #include "imgui.h"
-#include "bindings/imgui_impl_glfw.h"
-#include "bindings/imgui_impl_opengl3.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
 #if defined(IMGUI_IMPL_OPENGL_ES2)
 #include <GLES2/gl2.h>
