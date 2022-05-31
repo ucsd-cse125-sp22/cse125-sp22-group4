3f6e7fc5fb3a9971ecb65e647b2d000228995f43
amber3536 <36279682+amber3536@users.noreply.github.com>
2022-05-31 15:11:53 -0700

    putting if statement back

--- Client/src/Client.cpp
+++ Client/src/Client.cpp
@@ -1191,7 +1191,7 @@ void Client::finalDestGUI() {
 }
 
 void Client::stationaryItemGUI() {
-    if (gameEnded == 1) // || my_id == 0) // don't display on game over or if cat
+    if (gameEnded == 1 || my_id == 0) // don't display on game over or if cat
         return;
  
     ImGuiWindowFlags flags = 0;
