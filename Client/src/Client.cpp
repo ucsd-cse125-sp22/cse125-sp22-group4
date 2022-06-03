3b4087dcefbabe74fee495cae50ceb7b5a666bad
Jie Xu <jjx003@UCSD.EDU>
2022-06-03 12:39:22 -0700

    a

--- Client/src/Client.cpp
+++ Client/src/Client.cpp
@@ -713,15 +713,16 @@ void Client::displayCallback() {
 
         int ids[3] = { m1_id, m2_id, m3_id };
 
-        if (movingState[cat_id] == true) {
+        if (movingState[CAT] == true) {
             calcFinalBoneMatrix(catwalkinganimator);
         }
         else {
+            printf("not walk\n");
             calcFinalBoneMatrix(catanimator);
         }
         cat->draw(currCam->viewProjMat, identityMat, shader);
 
-        if (movingState[m1_id] == true) {
+        if (movingState[M1] == true) {
             calcFinalBoneMatrix(mousewalkinganimator1);
         }
         else {
@@ -729,7 +730,7 @@ void Client::displayCallback() {
         }
         mouse1->draw(currCam->viewProjMat, identityMat, shader);
 
-        if (movingState[m2_id] == true) {
+        if (movingState[M2] == true) {
             calcFinalBoneMatrix(mousewalkinganimator2);
         }
         else {
@@ -737,7 +738,7 @@ void Client::displayCallback() {
         }
         mouse2->draw(currCam->viewProjMat, identityMat, shader);
 
-        if (movingState[m3_id] == true) {
+        if (movingState[M3] == true) {
             calcFinalBoneMatrix(mousewalkinganimator3);
         }
         else {
@@ -840,16 +841,17 @@ void Client::idleCallback(float dt) {
         trailModel = mouse3->getModel() * glm::translate(glm::mat4(1), glm::vec3(0, 0.4, 0));
         glm::vec3 micepos3 = trailModel[3];
 
-        if (movingState[cat_id] == true) {
+        if (movingState[CAT] == true) {
             cattrailparticles->update(dt, 2, catpos);
             catwalkinganimator->update(dt);
         }
         else {
+            printf("not walk\n");
             cattrailparticles->update(dt, 0, catpos);
             catanimator->update(dt);
         }    
         
-        if (movingState[m1_id] == true) {
+        if (movingState[M1] == true) {
             micetrailparticles1->update(dt, 2, micepos1);
             mousewalkinganimator1->update(3*dt);
         }
@@ -858,7 +860,7 @@ void Client::idleCallback(float dt) {
             mouseanimator1->update(dt);
         }
     
-        if (movingState[m2_id] == true) {
+        if (movingState[M2] == true) {
             micetrailparticles2->update(dt, 2, micepos2);
             mousewalkinganimator2->update(3*dt);
         }
@@ -867,7 +869,7 @@ void Client::idleCallback(float dt) {
             mouseanimator2->update(dt);
         }
 
-        if (movingState[m3_id] == true) {
+        if (movingState[M3] == true) {
             micetrailparticles3->update(dt, 2, micepos3);
             mousewalkinganimator3->update(3*dt);
         }
@@ -2196,10 +2198,10 @@ void Client::setGameOver(int g, int w) {
 }
 
 void Client::setMovingState(PlayerState* playerstate) {
-    for (int i = 0; i < PLAYER_NUM; i++) {
-        movingState[i] = playerstate[i].moving;
-        //printf("moving state %d is %d\n", i, movingState[i]);
-    }
+    movingState[CAT] = playerstate[playerSelection[CAT]].moving;
+    movingState[M1] = playerstate[playerSelection[M1]].moving;
+    movingState[M2] = playerstate[playerSelection[M2]].moving;
+    movingState[M3] = playerstate[playerSelection[M3]].moving;
 }
 
 void Client::setAliveStatus(PlayerState* playerstate) {
