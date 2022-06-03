bd70e9bfcf53250ff1e3b13c2575559c9abca3f0
amber3536 <36279682+amber3536@users.noreply.github.com>
2022-06-02 19:16:44 -0700

    oops typo

--- Client/src/Client.cpp
+++ Client/src/Client.cpp
@@ -1446,8 +1446,8 @@ void Client::restore() {
         playerSelection[i] = NONE;
 
     displayStart = 1;
-    pair1 = 0;
-    pair2 = 0;
+    pairs1 = 0;
+    pairs2 = 0;
     gameStarted = 0;
     gameEnded = 0;
     catWon = 0;
